8d2b806371dde55c52babc891d5ec79fd9c3e01b
soulsoft <saadi.soulaimane@gmail.com>
2024-02-21 11:28:51 +0100

    Implementing Splash screen

---
+++ screens/SplashStack/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,43 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React, { FC, useEffect } from 'react';
+import { SafeAreaView, StatusBar, View, Text } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import { useAppDispatch } from '../../../state/redux-hooks';
+import { SplashLogo } from '../../../config/images';
+import styles from './styles';
+import {
+  endSplashLoading, } from "../../../state/reducers/user";
+
+
+const SplashScreen: FC = ({ navigation }: any) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+
+    setTimeout(()=>{
+      dispatch(endSplashLoading());
+    }, 2000)
+
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#000000',
+        alignItems: 'center',
+      }}>
+      <StatusBar barStyle="light-content" backgroundColor="#000000" />
+      <View style={styles.container}>
+        <SvgXml style={styles.splashLogoStyle} xml={SplashLogo.toString() } width={200} height={60}/>
+        <Text style={{ color: '#ffffff', fontSize:25,alignSelf: 'center'}}>Quiz</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SplashScreen;
