ffd02090a3ef6055fa528dcdb8bd0d63faed8504
soulsoft <saadi.soulaimane@gmail.com>
2024-02-22 11:35:38 +0100

    Showing a loading spinner while the map is loading - Implementing a loading spinner to indicate map loading. - Ensuring smooth user experience during map retrieval.

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,25 +1,27 @@
-import React, {FC, useEffect, useState, useRef, Fragment} from 'react';
+import React, { FC, useEffect, useRef, useState } from "react";
 import {
   View,
   SafeAreaView,
   StatusBar,
-  Text,
-  Platform,
-  TouchableOpacity,
-  Linking,
-  ScrollView,
-  Modal,
-  ActivityIndicator,
-  Image,
-} from 'react-native';
+  Text, ActivityIndicator, StyleSheet
 
+} from "react-native";
+
+import MapView, { MapMarker, Marker } from "react-native-maps";
 import {useAppDispatch, useAppSelector} from '../../state/redux-hooks';
+import { fetchProviderss } from "../../state/reducers/providers";
 
 const HomeScreen: FC = ({navigation}: any) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const dispatch = useAppDispatch();
   const {userInfo} = useAppSelector(state => state.user);
-  useEffect(() => {}, []);
+  const {providers} = useAppSelector(state => state.providers);
+  useEffect(() => {
 
+    //dispatch(fetchProviderss());
+  }, []);
+  const markerRef = useRef<MapMarker>(null);
   return (
     <SafeAreaView
       style={{
@@ -33,9 +35,58 @@ const HomeScreen: FC = ({navigation}: any) => {
         translucent
         backgroundColor="transparent"
       />
-      <View>
-        <Text>Welcome screen</Text>
-      </View>
+
+        <MapView
+          onMapLoaded={()=>{
+            setIsLoading(false);
+          }}
+          initialRegion={{
+            latitude: 51.9225,
+            longitude: 4.4792,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1,
+          }}
+          style={{
+            flex:1,
+            marginTop:30,
+            width: 400,
+            height: 300,
+          }}
+        >{providers.map(vehicle => (
+          <Marker
+            ref={markerRef}
+            tracksViewChanges={false}
+            key={vehicle.vehicle_id}
+            coordinate={{
+              latitude: vehicle.lat,
+              longitude: vehicle.lon,
+            }}
+            title={'Check | Max range: '+ (vehicle.current_range_meters/1000) + 'KM'}
+          ><View
+            style={{alignItems: 'center',
+              justifyContent: 'center', backgroundColor: "black",borderRadius:20, width:30, height:30, padding: 10, borderColor: 'white'}}>
+              <View style={{width:10, height:10,
+                position: 'absolute',
+                top: -1,
+                right: 15,
+                borderRadius: 10,
+                backgroundColor: vehicle.is_reserved ?'#D96363': 'green',
+                zIndex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+            </View>
+            <Text style={{color: 'white',textAlign: 'center', fontSize:9, fontWeight: '900',alignSelf: 'stretch'}}>C</Text>
+          </View></Marker>
+        ))}</MapView>
+      {isLoading && (<View style={{
+          ...StyleSheet.absoluteFillObject,
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>)}
     </SafeAreaView>
   );
 };
